3d24eace5245a9a6d7da938b11f896932f0b0f95
Federico Ulfo <federicoulfo@gmail.com>
2017-08-17 16:59:23 -0400

    Update upload

--- conf/service.py
+++ conf/service.py
@@ -1,5 +1,8 @@
 import os
 
-GLIMPSE_SERVICE_ASSET = os.getenv("GLIMPSE_SERVICE_ASSET", "asset.glimpse.online")
+GLIMPSE_SERVICE_ASSET = os.getenv("GLIMPSE_SERVICE_ASSET", "http://asset.glimpse.online")
 
 IMAGES_URL_BULK_URL = GLIMPSE_SERVICE_ASSET + "/images-url-bulk"
+
+
+
